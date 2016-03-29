e15023ea81c4d53c00a7f9195a5e01a7d0fa1e7f
jmartin <jmartin@zikzakmedia.com>
2016-03-29 12:00:38 +0200

    Add python 3 support

--- product.py
+++ product.py
@@ -4,10 +4,10 @@
 from trytond.pool import PoolMeta
 
 __all__ = ['Template']
-__metaclass__ = PoolMeta
 
 
 class Template:
+    __metaclass__ = PoolMeta
     __name__ = 'product.template'
 
     @classmethod
